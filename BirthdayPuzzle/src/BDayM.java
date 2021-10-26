20c4824173ac1d83f4a7e0fcad325f5b9c41454e
Sage Silberman <ssilberman2023@chadwickschool.org>
2021-10-25 19:41:02 -0700

    style: Add Comments

--- BirthdayPuzzle/src/BDayM.java
+++ BirthdayPuzzle/src/BDayM.java
@@ -1,12 +1,32 @@
+/**
+ * @author sagesilberman
+ *
+ */
 public class BDayM {
 	private int roomFolks; // people count in room
 	private int duplicates; // people duplicate of b-days
 
+	/**
+	 * Constructs an BDayM with the number of people in a room and number of
+	 * duplicate birthdays in that room
+	 * 
+	 * @param people the number of people in room
+	 * @param dup    the number of duplicate birthday's
+	 */
 	public BDayM(int people, int dup) {
 		roomFolks = people;
 		duplicates = dup;
 	}
 
+	// might need to fix the code true
+	/**
+	 * Returns <code>true</code> if the random birthday being tested is the same as
+	 * the ones set to true
+	 * 
+	 * @return <code>true</code> if the random birthday being tested is the same as
+	 *         the ones set to true ; <code>false</code> otherwise is false
+	 * 
+	 */
 	public boolean runTest() {
 		int[] days = new int[30]; // range of score board
 		// initially set all days to 0
@@ -26,6 +46,11 @@ public class BDayM {
 		return false;
 	}
 
+	/**
+	 * Returns a random number between 0 and 30
+	 * 
+	 * @return random number between 0 and 30
+	 */
 	private int genDay() {
 		// no leap year - just gen a random day 0-364
 		return ((int) (30 * Math.random()));
